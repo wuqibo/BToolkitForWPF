b2a6e804c3d810652d87c3d826b2d7aa10ddc8db
wuqibo <wqb55@qq.com>
2019-07-15 22:08:46 +0800

    no description

---
+++ Email.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace BToolkitForWPF
+{
+    class Email
+    {
+        public static void SendEmail()
+        {
+            try
+            {
+                MailMessage mailmessage = new MailMessage("wqb2001@163.com", "wqb55@qq.com", "《图影》服务器器异常", "《图影》服务器器异常，请立即排查！");
+                mailmessage.Priority = MailPriority.High; //邮件优先级
+                SmtpClient smtpClient = new SmtpClient("smtp.163.com", 25); //smtp地址以及端口号
+                smtpClient.Credentials = new NetworkCredential("wqb2001", "xxxxxx");//smtp用户名密码
+                smtpClient.EnableSsl = true; //启用ssl
+                smtpClient.Send(mailmessage);
+            }
+            catch (SmtpException ex)
+            {
+                Console.WriteLine("===============" + ex);
+            }
+        }
+    }
+}
